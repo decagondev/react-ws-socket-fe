06eb452916128cd5be0d35ede1665c86f405ec54
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-08 11:36:37 +0000

    refactored state and tyep in to Websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useContext, useState } from 'react';
 import './App.css'
 import { Websocket } from './components/Websocket';
-import { WebsocketContext } from './contexts/WebsocketContext';
-
-type MessageType = { msg: string, content: string };
 
 function App() {
-  const [value, setValue] = useState('');
-  const [messages, setMessages] = useState<MessageType[]>([]);
-  const socket = useContext(WebsocketContext);
+
 
   return (
     <div>
--- src/components/Websocket.tsx
+++ src/components/Websocket.tsx
@@ -1,4 +1,12 @@
+import { useContext, useState } from 'react';
+import { WebsocketContext } from '../contexts/WebsocketContext';
+
+type MessageType = { msg: string, content: string };
+
 export const Websocket = () => {
+    const [value, setValue] = useState('');
+    const [messages, setMessages] = useState<MessageType[]>([]);
+    const socket = useContext(WebsocketContext);
 
     return(
         <div>
